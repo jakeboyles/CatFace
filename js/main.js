9ef4a9797ff0a621e78d918636cfd8ea8304af3a
Jake Boyles <jake@infotrustllc.com>
2015-01-21 21:32:04 -0500

    inital

---
+++ js/main.js
@@ -0,0 +1,118 @@
+var canvas = new fabric.Canvas('c',{backgroundColor : "#0ff",width: '600',height: '500'});
+console.log(canvas);
+canvas.add(
+);
+
+
+    fabric.Image.fromURL("img/thompson.png", function(obj) {
+    canvas.add(obj.set({
+        width: 394,
+        hasControls: true,
+        //cornerColor: 'green',cornerSize: 16,transparentCorners: false,
+        selection: false,       
+        lockRotation:false,
+        //lockMovement: false,lockMovementY: false,lockMovementX: false,
+        //lockUniScaling: false,lockScalingY:false, lockScalingX:false,
+        transparentBorder: true,
+        height: 394,
+        angle: 0,
+        cornersize: 10,
+        left: 102, 
+        top: 52
+    }));
+    canvas.setActiveObject(canvas.item(0));
+    //img.bringToFront();
+    canvas.renderAll();
+    });
+
+
+var img = new Image();
+img.onload = function(){
+   canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            backgroundImageStretch: true,
+        });
+};
+img.src = "img/cat2.png";
+
+
+
+$(".cat1").on("click",function(){
+
+  var img = new Image();
+  img.onload = function(){
+   canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            backgroundImageStretch: true,
+        });
+  };
+  img.src = "img/cat1.jpg";
+  canvas.renderAll();
+
+})
+
+$(".cat2").on("click",function(){
+
+  var img = new Image();
+  img.onload = function(){
+   canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            backgroundImageStretch: true,
+        });
+  };
+  img.src = "img/cat2.png";
+  canvas.renderAll();
+
+})
+
+$(".cat3").on("click",function(){
+
+  var img = new Image();
+  img.onload = function(){
+   canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            backgroundImageStretch: true,
+        });
+  };
+  img.src = "img/cat3.png";
+  canvas.renderAll();
+
+})
+
+
+$(".cat4").on("click",function(){
+
+  var img = new Image();
+  img.onload = function(){
+   canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            backgroundImageStretch: true,
+        });
+  };
+  img.src = "img/cat4.png";
+  canvas.renderAll();
+
+})
+
+$(".save").on("click",function(){
+  canvasURL = canvas.toDataURL();
+var image = new Image();
+image.src = canvas.toDataURL("image/png");
+$('.saved').replaceWith(image);
+return false;
+})
